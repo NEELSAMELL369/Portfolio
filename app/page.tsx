c892eb4ec5409f9e44bbeeb7cf1b884560af02c6
NEELSAMEL23 <neelsamel3@gmail.com>
2025-05-27 21:24:06 +0530

    code updated 3 projects added

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 
+import About from "./about/page";
 import Hero from "./components/Hero";
+import Contact from "./contact/page";
+import Projects from "./projects/page";
 
-import Projects from "./components/Projects";
+
 
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
       <Projects />
- 
-     
+      <Contact />
     </main>
   );
 } 
--- contents/projects.ts
+++ contents/projects.ts
@@ -3,12 +3,29 @@ import { Project } from "@/types";
 
 export const projects: Project[] = [
     {
-        title: 'E-commerce Platform',
-        description: 'A full-stack e-commerce platform built with Next.js, TypeScript, and Tailwind CSS.',
-        technologies: ['Next.js', 'TypeScript', 'Tailwind CSS', 'Stripe'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
-        image: '/projects/e-commerce-website.png',
-      },
-      
-  ];
+        title: 'Task Manager App',
+        description: 'MERN task manager with JWT auth, roles, subtasks, file uploads, multi-user tasks, and dashboards.',
+        technologies: ["React", "Tailwind CSS", "React Router", "Moment.js", "Node.js", "Express.js", "MongoDB", "Mongoose", "React Context", "JWT", "bcrypt", "Multer", "Recharts", "Vercel", "Render"],
+        githubLink: 'https://github.com/NEELSAMEL23/Task_Manager',
+        demoLink: 'https://task-manager-dusky-nine.vercel.app/',
+        image: '/images/task-managment.avif'
+    },
+    {
+        title: 'Portfolio Website',
+        description: 'Responsive portfolio using Next.js App Router, with contact API, animations, and modern reusable components.',
+        technologies: ['Next.js', 'React', 'Framer Motion', 'Heroicons', 'React Icons', 'Nodemailer', 'Dotenv'],
+        githubLink: 'https://github.com/NEELSAMEL23/Portfolio',
+        demoLink: 'https://task-manager-dusky-nine.vercel.app/',
+        image: '/images/portfolio.jpg'
+
+    },
+    {
+        title: 'Search Movies App',
+        description: 'Responsive Movies Gallery app with filtering, sorting, and pagination using HTML, Tailwind CSS, and JavaScript.',
+        technologies: ["HTML5","Tailwind CSS","JavaScript (ES6)","Firebase Realtime Database"],
+        githubLink: 'https://github.com/NEELSAMEL23/Movies_Gallery',
+        demoLink: 'https://neelsamel23.github.io/Movies_Gallery/',
+        image: '/images/movie.png'
+    },
+
+];  
